c6c3d15d17d9714e5046b0a690f078436fda418b
aidyninho <ryzyk.aidyn@gmail.com>
2023-03-18 15:43:41 +0600

    passes all tests.

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,39 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try (BufferedReader bufferedReader = new BufferedReader(new java.io.FileReader(file))) {
+            String word;
+            while ((word = bufferedReader.readLine()) != null) {
+                String key = word.split(" ")[0];
+                String value = word.split(" ")[1];
+                switch (key) {
+                    case "Name:":
+                        profile.setName(value);
+                        break;
+                    case "Age:":
+                        profile.setAge(Integer.parseInt(value));
+                        break;
+                    case "Email:":
+                        profile.setEmail(value);
+                        break;
+                    case "Phone:":
+                        profile.setPhone(Long.parseLong(value));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+        return profile;
     }
 }
